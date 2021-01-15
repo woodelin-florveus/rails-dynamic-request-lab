8c9fb87fc62db9ea11c4c8d104c7283add23a7a9
woodelin-florveus <woodelinflorveus@gmail.com>
2021-01-15 16:51:21 -0500

    commit tree d4848ee7438d7f2120f2a01d9f0ec7f40535196d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,17 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show 
+    @student = Student.find(params[:id])
+  end
+
+  private
+
+  def set_student
+   
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
+  # resources :students, only: :index
+
+  # 1. create a route with an index action to list all students
+
+  get '/students', to: 'students#index'
+
+    # 2. create a route with a show action to render in my html
+
+  get '/students/:id', to: 'students#show'
+
 end
